4032dea10dd31a1ad0806a0c35d393f68c0c6c23
Abdul Rehman <nodebazaar@gmail.com>
2021-05-30 15:36:57 +0500

    some

--- assets/mains/detail.js
+++ assets/mains/detail.js
@@ -74,7 +74,7 @@ Downloads(title);
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download','apk-qubStore.apk'); //or any other extension
+    link.setAttribute('download','apk-Qub-Store.apk'); //or any other extension
     document.body.appendChild(link);
     link.click();
  }).catch(error=>{
